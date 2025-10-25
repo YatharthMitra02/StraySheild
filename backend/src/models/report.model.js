03588731b179028c90e5cf4f83613fbc0b112fd3
YatharthMitra02 <yatharthmitra28@gmail.com>
2025-10-25 14:00:01 +0530

    first commit

---
+++ backend/src/models/report.model.js
@@ -0,0 +1,23 @@
+import mongoose from ' mongoose'
+const reportSchema = new mongoose.reportASchema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    file:
+    {
+        type :String,
+        required : true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+const Report  = mongoose.model("Report", reportSchema);
+export default Report ;
